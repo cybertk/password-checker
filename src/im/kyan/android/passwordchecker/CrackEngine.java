e93a627cb069bb59cf5143dd867c0004ae044c38
kyan.ql.he <kyan.ql.he@48cdd3aa-0f50-69fb-adbc-d56a69cba100>
2011-04-14 09:07:31 +0000

    Merge branch 'master' into merge
    Conflicts:
    	AndroidManifest.xml

--- src/im/kyan/android/passwordchecker/CrackEngine.java
+++ src/im/kyan/android/passwordchecker/CrackEngine.java
@@ -30,7 +30,7 @@
  ** naughty words. Don"t look at it if you"re easily
  ** offended.
  ***************************************************************/
-package im.kyan.android.crackmypassword;
+package im.kyan.android.passwordchecker;
 
 import android.util.Log;
 
